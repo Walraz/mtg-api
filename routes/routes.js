d226953f168373e28285fb4a2d7287dd2d0df180
Walraz <hallstrom.rasmus@gmail.com>
2016-07-14 21:31:40 +0200

    lol

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app, pool, moment, io) {
         if(!err) {
           var data = rows
           data.forEach(function(row) {
-            row.Time_Created = moment(row.Time_Created).format('YYYY-MM-DD HH:mm:ss')
+            row.Time_Created = moment.utc(row.Time_Created).format('YYYY-MM-DD HH:mm:ss')
           })
           res.send(data)
         } else {
@@ -27,7 +27,7 @@ module.exports = function(app, pool, moment, io) {
           connection.release()
           if(!err) {
             var data = rows[0]
-            data.Time_Created = moment(data.Time_Created).format('YYYY-MM-DD HH:mm:ss')
+            data.Time_Created = moment.utc(data.Time_Created).format('YYYY-MM-DD HH:mm:ss')
             io.emit('create match', data)
             res.send(data)
           } else {
